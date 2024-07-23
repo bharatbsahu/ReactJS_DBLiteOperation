e1646f078375b8bfe8d2bb89723942360e2e8cb8
smartbharat@gmail.com <smartbharat@gmail.com>
2024-07-23 20:12:26 +0530

    updated js

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,18 @@ export default function Page() {
     <div className="container mt-3">
       <ul>
         <li>
-          <a href="http://localhost:3000/products-1-client-only-with-state">
-            clip 7 demo - client only with state
-          </a>
-        </li>
+              <a href="http://localhost:3000/products-1-client-only-with-state">
+                clip 7 demo - client only with state
+              </a>
+
+          </li>
+          <li>
+              <a href="http://localhost:3000/api/products">
+                clip 7 demo - API/Products
+              </a>
+
+          </li>
+       
       </ul>
     </div>
   );
